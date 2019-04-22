6e187b6ad7497cc74cbf7d51bc4da355e3075335
valeria-chemtai <valeriachemtai28@gmail.com>
2019-04-22 14:07:01 +0300

    data clean up

--- backend_system/flights/urls.py
+++ backend_system/flights/urls.py
@@ -5,17 +5,9 @@ from flights import views
 
 urlpatterns = [
     url(r'^flights/$', views.FlightViewSet.as_view(
-        actions={'post': 'create', 'get': 'list'}), name='flight-list'),
-    url(r'^flights/(?P<pk>[0-9]+)/$', views.FlightViewSet.as_view(
-        actions={'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='flight-detail'),
+        actions={'post': 'create'}), name='flight-list'),
     url(r'^allowed-destinations/$', views.LocationViewSet.as_view(
-        actions={'post': 'create', 'get': 'list'}), name='destination-list'),
-    url(r'^allowed-destinations/(?P<pk>[0-9]+)/$', views.LocationViewSet.as_view(
-        actions={'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
-        name='destination-detail'),
+        actions={'post': 'create'}), name='destination-list'),
     url(r'^flights/(?P<flight_pk>[0-9]+)/seats/$', views.SeatViewset.as_view(
-        actions={'post': 'create', 'get': 'list'}), name='flight-seat-list'),
-    url(r'^flights/(?P<flight_pk>[0-9]+)/seats/(?P<pk>[0-9]+)/$', views.SeatViewset.as_view(
-        actions={'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
-        name='flight-seat-detail'),
+        actions={'post': 'create'}), name='flight-seat-list'),
 ]
