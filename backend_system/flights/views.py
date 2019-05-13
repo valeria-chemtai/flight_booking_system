c4b853b0034fa81f92808411d2a274c8a844ffb6
valeria-chemtai <valeriachemtai28@gmail.com>
2019-05-14 01:09:55 +0300

    viewset

--- backend_system/flights/views.py
+++ backend_system/flights/views.py
@@ -2,7 +2,9 @@ from rest_framework.exceptions import NotFound
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import detail_route
 
+from bookings.models import Booking
 from flights.models import Flight, Location, Seat
 from flights.serializers import (
     FlightSerializer,
