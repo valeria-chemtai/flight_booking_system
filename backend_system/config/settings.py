7d00a579abe77c9e67dc61ebd7ca7effa744b919
valeria-chemtai <valeriachemtai28@gmail.com>
2019-05-14 23:45:33 +0300

    merge migrations

--- backend_system/config/settings.py
+++ backend_system/config/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    # 'EXCEPTION_HANDLER': 'exception_handler.exception_handler'
+    'EXCEPTION_HANDLER': 'exception_handler.exception_handler'
 }
 
 ROOT_URLCONF = 'config.urls'
