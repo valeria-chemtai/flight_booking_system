27cdf93b9295b385d178987aa62206f2e9ea847f
Valeria Chemtai <valeriachemtai28@gmail.com>
2019-05-20 23:16:56 +0300

    rq (#31)
    * rq scheduler
    
    * other priorities
    
    * deactivate
    
    * fix
    
    * queue job
    
    * redis

--- backend_system/config/settings.py
+++ backend_system/config/settings.py
@@ -71,6 +71,7 @@ RQ_QUEUES = {
     'default': {
         'HOST': env.str('DJANGO_RQ_HOST', 'localhost'),
         'PORT': env.str('DJANGO_RQ_PORT', 6379),
+        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379'),
         'DB': env.str('DJANGO_RQ_DB', 0),
         'PASSWORD': env.str('DJANGO_RQ_PASSWORD', ''),
         'DEFAULT_TIMEOUT': 600,
